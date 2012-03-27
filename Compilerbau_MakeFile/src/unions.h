7587ae4e6aa7bb1695f90a0c49c694c06c6982a3
sebastian <sebastianboehm91@gmail.com>
2012-03-27 14:54:43 +0200

    unions.h hinzugefügt, wird für parser-erweiterung benötigt. außerdem erstes beispiel für Erweiterung implementiert

---
+++ Compilerbau_MakeFile/src/unions.h
@@ -0,0 +1,20 @@
+/*
+ * unions.h
+ *
+ *  Created on: 27.03.2012
+ *      Author: sebastian
+ */
+
+#ifndef UNIONS_H_
+#define UNIONS_H_
+
+struct functpar{
+	int type;
+	char *name;
+};
+
+
+
+#endif /* UNIONS_H_ */
+
+
